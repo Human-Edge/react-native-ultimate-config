b9f501b840ab4f6be43117877074f5dd551d4aab
sonali-humanedge <sonali@humanedge.co>
2022-10-18 14:42:46 +0530

    fix: package name in cli file

--- packages/react-native-ultimate-config/src/cli.js
+++ packages/react-native-ultimate-config/src/cli.js
@@ -10,7 +10,11 @@ module.exports = async function () {
   const project_root = yargs.argv.projectRoot;
   yargs.default(
     "libRoot",
-    path.join(project_root, "node_modules", "react-native-ultimate-config")
+    path.join(
+      project_root,
+      "node_modules",
+      "react-native-ultimate-config-patch"
+    )
   );
   const lib_root = yargs.argv.libRoot;
 
